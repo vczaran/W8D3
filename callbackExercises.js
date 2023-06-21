ba53b424cc38ed4a67d90f8e8d1ad5ecd9bc6601
Joe Randazzo <jprandazzo@icloud.com>
2023-06-21 15:27:31 -0400

    addnumbers

--- callbackExercises.js
+++ callbackExercises.js
@@ -1,43 +1,43 @@
-class Clock {
-  constructor() {
-    // 1. Create a Date object.
-    let date = new Date();
-    // 2. Store the hours, minutes, and seconds.
-    this.hours = date.getHours();
-    this.minutes = date.getMinutes();
-    this.seconds = date.getSeconds();
-    // 3. Call printTime.
-    this.printTime();
-    // 4. Schedule the tick at 1 second intervals.
-    setInterval(this._tick.bind(this), 1000);
-  };
+// class Clock {
+//   constructor() {
+//     // 1. Create a Date object.
+//     let date = new Date();
+//     // 2. Store the hours, minutes, and seconds.
+//     this.hours = date.getHours();
+//     this.minutes = date.getMinutes();
+//     this.seconds = date.getSeconds();
+//     // 3. Call printTime.
+//     this.printTime();
+//     // 4. Schedule the tick at 1 second intervals.
+//     setInterval(this._tick.bind(this), 1000);
+//   };
 
-   printTime() {
-    // Format the time in HH:MM:SS
-    console.log(this.hours + ":" + this.minutes + ":" + this.seconds);
-    // Use console.log to print it.
-  };
+//    printTime() {
+//     // Format the time in HH:MM:SS
+//     console.log(this.hours + ":" + this.minutes + ":" + this.seconds);
+//     // Use console.log to print it.
+//   };
 
-  _tick() {
-    // 1. Increment the time by one second.
-    this.seconds += 1;
+//   _tick() {
+//     // 1. Increment the time by one second.
+//     this.seconds += 1;
 
-    if (this.seconds === 60) {
-        this.minutes += 1;
-        this.seconds = 0;
-    };
+//     if (this.seconds === 60) {
+//         this.minutes += 1;
+//         this.seconds = 0;
+//     };
 
-    if (this.minutes === 60) {
-        this.hours += 1;
-        this.minutes = 0;
-    };
+//     if (this.minutes === 60) {
+//         this.hours += 1;
+//         this.minutes = 0;
+//     };
 
-    // 2. Call printTime.
-    this.printTime();
-  };
-}
+//     // 2. Call printTime.
+//     this.printTime();
+//   };
+// }
 
-const clock = new Clock();
+// const clock = new Clock();
 
 
 
@@ -50,17 +50,18 @@ const reader = readline.createInterface({
 
 function addNumbers(sum, numsLeft, completionCallback) {
 
-  // completionCallback ||= sum => console.log('Total Sum: ${sum}')
   if (numsLeft > 0) {
-    reader.question('Type in a number'), function(res) {
+    reader.question('Type in a number: ', function(res) {
       sum += parseInt(res);
       console.log(sum);
-    };
-    addNumbers(sum,numsLeft-1,completionCallback);
-    
+      numsLeft--;
+      addNumbers(sum,numsLeft,completionCallback);
+    });
   }
   else {
-    completionCallback(sum);
     reader.close();
+    completionCallback(sum);
   };
 }
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
